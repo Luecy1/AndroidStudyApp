f0f9d22c1f6cbfacd21f1e89a6608373b8b862bb
luecy1 <kowaheno@gmail.com>
2018-02-26 23:31:05 +0900

    今日の進捗をコミット

--- app/src/androidTest/java/com/github/luecy1/androidstudyapp/ui/user/UserFragmentTest.java
+++ app/src/androidTest/java/com/github/luecy1/androidstudyapp/ui/user/UserFragmentTest.java
@@ -1,7 +1,9 @@
 package com.github.luecy1.androidstudyapp.ui.user;
 
 import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 
 import com.github.luecy1.androidstudyapp.R;
 import com.github.luecy1.androidstudyapp.TestUtil;
@@ -9,20 +11,25 @@ import com.github.luecy1.androidstudyapp.binding.FragmentBindingAdapters;
 import com.github.luecy1.androidstudyapp.testing.SingleFragmentActivity;
 import com.github.luecy1.androidstudyapp.ui.common.NavigationController;
 import com.github.luecy1.androidstudyapp.util.EspressoTestUtil;
+import com.github.luecy1.androidstudyapp.util.RecyclerViewMatcher;
 import com.github.luecy1.androidstudyapp.util.ViewModelUtil;
 import com.github.luecy1.androidstudyapp.vo.Repo;
 import com.github.luecy1.androidstudyapp.vo.Resource;
 import com.github.luecy1.androidstudyapp.vo.User;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -36,7 +43,6 @@ import static org.mockito.Mockito.when;
 /**
  * Created by you on 2018/02/24.
  */
-// TODO
 public class UserFragmentTest {
 
     @Rule
@@ -92,5 +98,86 @@ public class UserFragmentTest {
         userData.postValue(Resource.loading(user));
         onView(withId(R.id.name)).check(matches(withText(user.name)));
         onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void loadedUser() {
+        User user = TestUtil.createUser("foo");
+        userData.postValue(Resource.success(user));
+        onView(withId(R.id.name)).check(matches(withText(user.name)));
+        onView(withId(R.id.progress_bar)).check(matches(not(user.name)));
+    }
+
+    @Test
+    public void loadRepos() {
+        List<Repo> repos = setRepos(2);
+
+        for (int pos = 0; pos < repos.size(); pos++) {
+            Repo repo = repos.get(pos);
+            onView(listMatcher().atPosition(pos)).check(
+                    matches(hasDescendant(withText(repo.name)))
+            );
+            onView(listMatcher().atPosition(pos)).check(
+                    matches(hasDescendant(withText(repo.description)))
+            );
+            onView(listMatcher().atPosition(pos)).check(
+                    matches(hasDescendant(withText("" + repo.stars)))
+            );
+        }
+        Repo repo3 = setRepos(3).get(2);
+        onView(listMatcher().atPosition(2)).check(
+                matches(hasDescendant(withText(repo3.name)))
+        );
+    }
+
+    @Test
+    public void clickRepo() {
+        List<Repo> repos = setRepos(2);
+        Repo selected = repos.get(1);
+        onView(withText(selected.description)).perform(click());
+        verify(navigationController).navigateToRepo(selected.owner.login, selected.name);
+    }
+
+    @Test
+    public void nullUser() {
+        userData.postValue(null);
+        onView(withId(R.id.name)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void nullRepoList() {
+        repoListData.postValue(null);
+        onView(listMatcher().atPosition(0)).check(doesNotExist());
+    }
+
+    @Test
+    public void nulledUser() {
+        User user = TestUtil.createUser("foo");
+        userData.postValue(Resource.success(user));
+        onView(withId(R.id.name)).check(matches(withText(user.name)));
+        userData.postValue(null);
+        onView(withId(R.id.name)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void nulledRepoList() {
+        setRepos(5);
+        onView(listMatcher().atPosition(1)).check(matches(isDisplayed()));
+        repoListData.postValue(null);
+        onView(listMatcher().atPosition(0)).check(doesNotExist());
+    }
+
+    @NonNull
+    private RecyclerViewMatcher listMatcher() {
+        return new RecyclerViewMatcher(R.id.repo_list);
+    }
+
+    private List<Repo> setRepos(int count) {
+        List<Repo> repos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            repos.add(TestUtil.createRepo("foo", "name" + i, "desc" + i));
+        }
+        repoListData.postValue(Resource.success(repos));
+        return repos;
     }
 }
